2d182cb9bcf1ce72473aa5b690f07f74b6ba90e6
teacher <teacher@4PC0>
2023-10-07 20:39:46 +0300

    Реализовал создание библиотеки

--- ConsoleLibray.Core/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ConsoleLibray.Core
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ ConsoleLibray.UI/Models/Enums/StartMenuEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleLibray.UI.Models.Enums
+{
+    public enum StartMenuEnum
+    {
+        Create = 1,
+        Clear = 4,
+        Exit = 5
+    }
+}
